62cae5e59c88f0fe60935b27879036a769955dec
Tarantin <mr.fadeyka@gmail.com>
2023-02-28 21:47:55 +0300

    homework_1/ex_002 СДЕЛАЛ СХЕМУ И КОД

---
+++ homework/homework_1/ex_002/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int a = 22;
+int b = 3;
+int c = 9;
+//int max;
+
+if (a > b && a > c)
+{
+    Console.WriteLine(a);
+
+}
+else if (b > a && b > c)
+{
+    Console.WriteLine(b);
+}
+else
+    Console.WriteLine(c);
+
+
+
