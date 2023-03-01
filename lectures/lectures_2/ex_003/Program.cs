ff3d5755925b1f3614ef69694b7ee9a65f508768
Tarantin <mr.fadeyka@gmail.com>
2023-03-01 11:24:01 +0300

    ЛЕКЦИЯ 2

---
+++ lectures/lectures_2/ex_003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Имеется одномерный массив array из n элементов, требуется найти эллемент массива равный find.
+//              0   1   2   3   4
+int[] array = { 1, 21, 43, 67, 67 };
+int n = array.Length;
+int find = 67;
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] == find)
+    {
+        Console.WriteLine(array[i]);
+        break;
+    }
+    i++;
+}
---
+++ lectures/lectures_2/ex_004/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] collection)
+
+{
+    int length = collection.Length;
+    int i = 0;
+
+    while (i < length)
+    {
+        collection[i] = new Random().Next(1, 10);
+        i++;
+    }
+}
+
+void PrintArray(int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
